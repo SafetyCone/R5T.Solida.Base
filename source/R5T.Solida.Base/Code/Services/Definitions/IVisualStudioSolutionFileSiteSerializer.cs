363c305c7e8740ae1d64ade1465dbfbada2c8dce
MinexAutomation <minex.automation@gmail.com>
2020-02-03 22:40:10 -0800

    Added IVisualStudioSolutionFileSiteSerializer service definition.

---
+++ source/R5T.Solida.Base/Code/Services/Definitions/IVisualStudioSolutionFileSiteSerializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using R5T.Cambridge.Types;
+
+
+namespace R5T.Solida
+{
+    public interface IVisualStudioSolutionFileSiteSerializer
+    {
+        SolutionFileSite Deserialize(string solutionFilePath);
+        void Serialize(SolutionFileSite solutionFileSite, bool overwrite = true);
+    }
+}
